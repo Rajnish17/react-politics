0bdbe7e71a0bc7edd7fca820a3e2a0682416a209
Rajnish <RajnishAryan12@gmail.com>
2023-07-24 11:39:17 +0530

    hero and nav section added

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import HeroSection from "./Components/HeroSection";
+import Section2 from "./Components/Section2";
+import Section3 from "./Components/Section3";
+import Section4 from "./Components/Section4";
+import Section5 from "./Components/Section5";
+import Section6 from "./Components/Section6";
+
+
+// import {
+//   BrowserRouter,
+//   Route,
+//   Routes,
+// } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+     <Navbar />
+     <HeroSection/>
+     {/* <Section2/> */}
+     {/* <Section3/> */}
+     {/* <Section4/> */}
+     {/* <Section5/> */}
+     {/* <Section6/> */}
+     {/* <Footer/> */}
+
+
+
+
+
+
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navbar />} />
+          <HeroSection/>
+        </Routes>
+        <Footer/>
+      </BrowserRouter> */}
+
+
+    </>
+  );
+}
+
+export default App;
